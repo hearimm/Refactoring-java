7745adde02470ca5d0cd38c01e6c7fd3a82e2e9f
haerimm <doksul23@gmail.com>
2018-01-19 15:50:43 +0900

    extract method Price getCharge Switch case each method

--- src/com/company/Price.java
+++ src/com/company/Price.java
@@ -7,20 +7,36 @@ abstract class  Price {
     double result = 0;
     switch (getPriceCode()) {
       case Movie.REGULAR:
-        result += 2;
-        if (daysRented > 2) {
-          result += (daysRented - 2) * 1.5;
-        }
+        result = geResultRegular(daysRented);
         break;
       case Movie.NEW_RELEASE:
-        result += daysRented * 3;
+        result += getResultNew(daysRented);
         break;
       case Movie.CHILDRENS:
-        result += 1.5;
-        if (daysRented > 3) {
-          result += (daysRented - 3) * 1.5;
-        }
+        result = getResult(daysRented);
         break;
+    }
+    return result;
+  }
+
+  private double getResult(int daysRented) {
+    double result = 0;
+    result += 1.5;
+    if (daysRented > 3) {
+      result += (daysRented - 3) * 1.5;
+    }
+    return result;
+  }
+
+  private int getResultNew(int daysRented) {
+    return daysRented * 3;
+  }
+
+  private double geResultRegular(int daysRented) {
+    double result = 0;
+    result += 2;
+    if (daysRented > 2) {
+      result += (daysRented - 2) * 1.5;
     }
     return result;
   }
