0c7fb83438d45f99d2ebf36c35214ee159be26a5
haerimm <doksul23@gmail.com>
2018-01-19 11:51:39 +0900

    move method amountFor Customer -> Rental

--- src/com/company/Customer.java
+++ src/com/company/Customer.java
@@ -29,7 +29,7 @@ public class Customer {
       Rental each = (Rental) rentals.nextElement();
 
       // 비디오 종류별 대여료 계산
-      thisAmount = amountFor(each);
+      thisAmount = each.amountFor();
 
       //적립 포인트를 1 포인트 증가
       frequentRenterPoints++;
@@ -48,26 +48,6 @@ public class Customer {
     //푸터 행 추가
     result += "누적 대여료: " + String.valueOf(totalAmount) + "\n";
     result += "적립 포인트: " + String.valueOf(frequentRenterPoints);
-    return result;
-  }
-
-  private double amountFor(Rental aRental) {
-    double result = 0;
-    switch (aRental.getMovie().getPriceCode()) {
-      case Movie.REGULAR:
-        result += 2;
-        if (aRental.getDaysRented() > 2)
-          result += (aRental.getDaysRented() - 2) * 1.5;
-        break;
-      case Movie.NEW_RELEASE:
-        result += aRental.getDaysRented() * 3;
-        break;
-      case Movie.CHILDRENS:
-        result += 1.5;
-        if (aRental.getDaysRented() > 3)
-          result += (aRental.getDaysRented() - 3) * 1.5;
-        break;
-    }
     return result;
   }
 
