7af3648b010ee81bdfc4124db242acc9934fafee
haerimm <doksul23@gmail.com>
2018-01-19 16:20:36 +0900

    적립포인트 method Override

--- src/com/company/NewReleasePrice.java
+++ src/com/company/NewReleasePrice.java
@@ -11,4 +11,15 @@ public class NewReleasePrice extends Price {
   double getCharge(int daysRented) {
     return daysRented * 3;
   }
+
+  @Override
+  int getFrequentRenterPoints(int daysRented) {
+    // 적립 포인트를 1 포인트 증가
+    // 최신물을 이틀 이상 대여하면 보너스 포인트 지급
+    if (daysRented > 1) {
+      return 2;
+    } else {
+      return 1;
+    }
+  }
 }
--- src/com/company/Price.java
+++ src/com/company/Price.java
@@ -1,17 +1,12 @@
 package com.company;
 
-abstract class  Price {
+abstract class Price {
 
   abstract int getPriceCode();
+
   abstract double getCharge(int daysRented);
 
   int getFrequentRenterPoints(int daysRented) {
-    // 적립 포인트를 1 포인트 증가
-    // 최신물을 이틀 이상 대여하면 보너스 포인트 지급
-    if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1) {
-      return 2;
-    } else {
-      return 1;
-    }
+    return 1;
   }
 }
