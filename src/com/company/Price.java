a3e21f6a37870f105994103ff1a062846ec70628
haerimm <doksul23@gmail.com>
2018-01-19 16:14:50 +0900

    getCharge change abstract method

--- src/com/company/Price.java
+++ src/com/company/Price.java
@@ -1,41 +1,7 @@
 package com.company;
 
 abstract class  Price {
+
   abstract int getPriceCode();
-
-  double getCharge(int daysRented) {
-    double result = 0;
-    switch (getPriceCode()) {
-      case Movie.REGULAR:
-        result = geResultRegular(daysRented);
-        break;
-      case Movie.NEW_RELEASE:
-        result += getResultNew(daysRented);
-        break;
-      case Movie.CHILDRENS:
-        result = getResult(daysRented);
-        break;
-    }
-    return result;
-  }
-
-  private double getResult(int daysRented) {
-    double result = 1.5;
-    if (daysRented > 3) {
-      result += (daysRented - 3) * 1.5;
-    }
-    return result;
-  }
-
-  private double getResultNew(int daysRented) {
-    return daysRented * 3;
-  }
-
-  private double geResultRegular(int daysRented) {
-    double result = 2;
-    if (daysRented > 2) {
-      result += (daysRented - 2) * 1.5;
-    }
-    return result;
-  }
+  abstract double getCharge(int daysRented);
 }
