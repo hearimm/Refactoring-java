86e20df76942da4f7dcf9957f9f65f50db5bac0b
haerimm <doksul23@gmail.com>
2018-01-19 14:27:55 +0900

    move method 적립포인트 Customer -> Rental

--- src/com/company/Customer.java
+++ src/com/company/Customer.java
@@ -29,7 +29,7 @@ public class Customer {
       Rental each = (Rental) rentals.nextElement();
 
       // 비디오 종류별 대여료 계산
-      frequentRenterPoints += getFrequentRenterPoints(each);
+      frequentRenterPoints += each.getFrequentRenterPoints();
 
       // 이번에 대여하는 비디오 정보와 대여료를 출력
       result += "\t" + each.getMovie().getTitle() + "\t" +
@@ -44,14 +44,4 @@ public class Customer {
     return result;
   }
 
-  private int getFrequentRenterPoints(Rental each) {
-    // 적립 포인트를 1 포인트 증가
-    // 최신물을 이틀 이상 대여하면 보너스 포인트 지급
-    if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-        each.getDaysRented() > 1) {
-      return 2;
-    } else {
-      return 1;
-    }
-  }
 }
