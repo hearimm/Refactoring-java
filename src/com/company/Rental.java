318592830adf6a438dd305780e89c42c0abcfa1f
haerimm <doksul23@gmail.com>
2018-01-19 15:06:16 +0900

    move method getFrequentRenterPoints Rental -> Movie

--- src/com/company/Rental.java
+++ src/com/company/Rental.java
@@ -23,13 +23,6 @@ public class Rental {
   }
 
   int getFrequentRenterPoints() {
-    // 적립 포인트를 1 포인트 증가
-    // 최신물을 이틀 이상 대여하면 보너스 포인트 지급
-    if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-        getDaysRented() > 1) {
-      return 2;
-    } else {
-      return 1;
-    }
+    return _movie.getFrequentRenterPoints(_daysRented);
   }
 }
