1c3e0c6a8db1862ebe892837e3d67519574608bb
haerimm <doksul23@gmail.com>
2018-01-19 12:05:50 +0900

    extractMethod 적립포인트

--- src/com/company/Customer.java
+++ src/com/company/Customer.java
@@ -28,13 +28,7 @@ public class Customer {
       Rental each = (Rental) rentals.nextElement();
 
       // 비디오 종류별 대여료 계산
-
-      //적립 포인트를 1 포인트 증가
-      frequentRenterPoints++;
-      // 최신물을 이틀 이상 대여하면 보너스 포인트 지급
-      if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-          each.getDaysRented() > 1)
-        frequentRenterPoints++;
+      frequentRenterPoints += getFrequentRenterPoints(each);
 
       // 이번에 대여하는 비디오 정보와 대여료를 출력
       result += "\t" + each.getMovie().getTitle() + "\t" +
@@ -47,6 +41,17 @@ public class Customer {
     result += "누적 대여료: " + String.valueOf(totalAmount) + "\n";
     result += "적립 포인트: " + String.valueOf(frequentRenterPoints);
     return result;
+  }
+
+  private int getFrequentRenterPoints(Rental each) {
+    int frequentRenterPoints = 0;
+    //적립 포인트를 1 포인트 증가
+    frequentRenterPoints += 1;
+    // 최신물을 이틀 이상 대여하면 보너스 포인트 지급
+    if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
+        each.getDaysRented() > 1)
+      frequentRenterPoints += 1;
+    return frequentRenterPoints;
   }
 
 }
